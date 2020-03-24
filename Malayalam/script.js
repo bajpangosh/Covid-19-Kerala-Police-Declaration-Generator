69e4eea8382cbd8dad0a12b0c089d8f8500a4d02
Bajpan Gosh <bajpangosh@users.noreply.github.com>
2020-03-25 00:12:07 +0530

    Update script.js
    update

--- Malayalam/script.js
+++ Malayalam/script.js
@@ -8,8 +8,3 @@ app.controller('Controller', ['$scope', '$timeout',  function($scope, $timeout) 
       $scope.prefilled = 'https://blog.codepen.io/wp-content/uploads/2012/06/Black-Small.png'
     }, 2000)
 }])
-
-angular.module('datepickerBasicUsage', ['ngMaterial', 'ngMessages']).controller('AppCtrl', function() {
-  this.myDate = new Date();
-  this.isOpen = false;
-});
