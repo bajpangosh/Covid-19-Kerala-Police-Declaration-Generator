4f567e23f684823e155776a2a998179a8e284de0
Bajpan Gosh <bajpangosh@users.noreply.github.com>
2020-03-24 21:03:14 +0530

    Update Push
    Update Push

---
+++ script.js
@@ -0,0 +1,10 @@
+var app = angular.module('myApp', []);
+
+app.controller('Controller', ['$scope', '$timeout',  function($scope, $timeout) {
+    $scope.prefilled = 'https://blog.codepen.io/wp-content/uploads/2012/06/Black-Small.png'
+    $scope.logourl = $scope.logourl || $scope.prefilled
+    $timeout(function() {
+      // alert('changed')
+      $scope.prefilled = 'https://blog.codepen.io/wp-content/uploads/2012/06/Black-Small.png'
+    }, 2000)
+}])
